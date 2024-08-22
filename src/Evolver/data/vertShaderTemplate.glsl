50d4e3c81e684354534edabd1eb184ac00f54705
João Santos <42850719+JoaoMariaSantos@users.noreply.github.com>
2024-08-22 22:27:43 +0100

    Mac compatibility fixed
    Mac handles older OpenGL versions.
    GLSL shader version reverted to #version 100.
    Bug with camera on mac happening but fix is unknown.

--- src/Evolver/data/vertShaderTemplate.glsl
+++ src/Evolver/data/vertShaderTemplate.glsl
@@ -1,8 +1,10 @@
+
+
 uniform mat4 modelview;
 uniform mat4 projection;
-in vec4 position;
-in vec4 texCoord;
-out vec2 uv;
+attribute vec4 position;
+attribute vec4 texCoord;
+varying vec2 uv;
 void main() {
   uv = texCoord.xy;
   gl_Position = projection * modelview * position;
